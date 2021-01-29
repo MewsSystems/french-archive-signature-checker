80a30515ce24956d726bd09113351603cc385768
Marek Tresnak <marek.tresnak@outlook.com>
2021-01-29 14:03:36 +0100

    WIP

--- src/Archive.cs
+++ src/Archive.cs
@@ -94,17 +94,4 @@ namespace Mews.SignatureChecker
             });
         }
     }
-
-    internal sealed class ArchiveEntry
-    {
-        public ArchiveEntry(string name, string content)
-        {
-            Name = name;
-            Content = content;
-        }
-
-        public string Name { get; }
-
-        public string Content { get; }
-    }
 }
