6cd63c324ceb220d9a23571554f6f13f992b0775
Marek Tresnak <marek.tresnak@outlook.com>
2021-04-15 18:59:00 +0200

    Added support for develop key

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using FuncSharp;
@@ -12,32 +13,50 @@ namespace Mews.Fiscalization.SignatureChecker
     {
         public static void Main(string[] args)
         {
-            var path = args.SingleOption().ToTry(_ => "Invalid arguments".ToEnumerable());
-            var archiveFiles = path.FlatMap(p => ZipFileReader.Read(p));
+            var (optionArguments, pathArguments) = args.Partition(a => a.StartsWith("--"));
+
+            var archivePath = pathArguments.SingleOption().ToTry(_ => "Invalid arguments".ToEnumerable());
+            var archiveFiles = archivePath.FlatMap(p => ZipFileReader.Read(p));
             var archive = archiveFiles.FlatMap(files => Archive.Create(files));
 
-            var result = archive.Match(
-                a => IsArchiveValid(a).Match(
+            var cryptoServiceProvider = GetCryptoServiceProvider(optionArguments);
+
+            var result = Try.Aggregate(
+                archive,
+                cryptoServiceProvider,
+                (a, rsa) => IsArchiveValid(a, rsa).Match(
                     t => "Archive signature IS valid.",
                     f => "Archive signature IS NOT valid."
-                ),
-                e => e.MkLines()
+                )
             );
-            Console.WriteLine(result);
+            Console.WriteLine(result.Match(r => r, e => e.MkLines()));
         }
 
-        private static bool IsArchiveValid(Archive archive)
+        private static ITry<RSACryptoServiceProvider, IEnumerable<string>> GetCryptoServiceProvider(IEnumerable<string> optionArguments)
+        {
+            var useDevelopKey = optionArguments.Contains("--develop", StringComparer.InvariantCultureIgnoreCase);
+            var fileName = useDevelopKey.Match(
+                t => "DevelopPublicKey.xml",
+                f => "ProductionPublicKey.xml"
+            );
+            var cryptoServiceProvider = Try.Create<RSACryptoServiceProvider, Exception>(_ =>
+            {
+                var xmlKey = File.ReadAllText(fileName);
+                var rsa = new RSACryptoServiceProvider();
+                rsa.FromXmlString(xmlKey);
+                return rsa;
+            });
+            return cryptoServiceProvider.MapError(_ => "Missing or invalid key.".ToEnumerable());
+        }
+
+        private static bool IsArchiveValid(Archive archive, RSACryptoServiceProvider cryptoServiceProvider)
         {
             var computedSignature = ComputeSignature(archive);
-            var xmlKey = File.ReadAllText("PublicKey.xml");
-            var rsa = new RSACryptoServiceProvider();
-            rsa.FromXmlString(xmlKey);
-
             var hashAlgorithmName = archive.Metadata.Version.Match(
                 ArchiveVersion.v100, _ => HashAlgorithmName.SHA1,
                 ArchiveVersion.v400, _ => HashAlgorithmName.SHA256
             );
-            return rsa.VerifyData(computedSignature, archive.Signature.Value, hashAlgorithmName, RSASignaturePadding.Pkcs1);
+            return cryptoServiceProvider.VerifyData(computedSignature, archive.Signature.Value, hashAlgorithmName, RSASignaturePadding.Pkcs1);
         }
 
         private static byte[] ComputeSignature(Archive archive)
